45d6fbcd8304eaef7471f671dfe3e151fd915aa0
Cryptane Online <adityaarghya0@gmail.com>
2021-07-02 07:26:50 +0530

    Add files via upload

--- qr-generator.py
+++ qr-generator.py
@@ -1,13 +1,21 @@
 # This code generates a QR code for my Github profile
 # Modify the link to suit your needs
 
-def qr_generator():
+import sys
+
+
+def qr_generator(link):
     import pyqrcode
     from pyqrcode import QRCode
 
-    link = 'https://github.com/Sanya1001'
+    #link = 'https://github.com/Sanya1001'
     url = pyqrcode.create(link)
     url.svg('git.svg', scale=8)
 
-
-qr_generator()
+n=len(sys.argv)
+link = 'https://github.com/Sanya1001'
+if(n>1):
+	link=sys.argv[1]
+print ("Using link")
+print(link)
+qr_generator(link)
